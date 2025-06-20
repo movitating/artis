57154a35e073c7ee74a2228499d1b49f328e307c
youngjune <jjmullan24@gmail.com>
2025-06-20 23:35:49 +0900

    ✨ feat: UI 초안

--- src/shared/button/Menu-button.tsx
+++ src/shared/button/Menu-button.tsx
@@ -6,7 +6,7 @@ function MenuButton() {
       <svg width="32" height="8" viewBox="0 0 32 8" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
           d="M1 0.5H31M1 7.5H31"
-          stroke="black"
+          stroke="#f2f2f7"
           stroke-miterlimit="10"
           stroke-linecap="round"
           stroke-linejoin="round"
